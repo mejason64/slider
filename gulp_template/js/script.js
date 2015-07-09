5fd88e2790adfd2cded2931aa57cf80f12d550af
mejason64 <evansjason64@gmail.com>
2015-07-09 09:34:24 -0700

    second slider commit

--- gulp_template/js/script.js
+++ gulp_template/js/script.js
@@ -3,31 +3,47 @@
 		items = document.querySelectorAll('.slideshow figure'),
 		next = document.querySelector('.next'),
 		prev = document.getElementsByClassName('prev')[0];
-
 	var numItems = items.length;
 
+	var timer = setTimeout(cycle, 5000);
+
+	function cycle(){
+			next.click();
+	}
+
+	
+
+		function reset(){
+		timer = setTimeout(cycle, 5000);
+		}
+
+
+	
+		
 		function showCurrent(){
 			var itemsToShow = Math.abs(counter%numItems);
-				for (var i = 0; i < numItems; i++){
-					items[i].classList.remove('show');
-				}
-				items[itemsToShow].classList.add('show');
-
-		}
 			
-
-
-
-
+			for (var i = 0; i < numItems; i++){
+				items[i].classList.remove('show');
+			}
+			items[itemsToShow].classList.add('show');
+		}
 			next.addEventListener('click', function(){
+				clearTimeout(timer);
+				reset();
 				counter++;
 				showCurrent();
 			});
 			prev.addEventListener('click', function(){
+				clearTimeout(timer);
+				reset();
 				counter--;
 				showCurrent();
 			});
 
-		
+			//function makeScroll(){
+			//	next.click();
+			//} 
 
+			//setInterval(makeScroll, 6000);
 })();
